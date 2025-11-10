87bedd26200ef4f3734b3913d184da3fb07e678c
chriswolf <chriswolfdesign@gmail.com>
2025-11-10 07:07:03 -0500

    Adds Experience section to webpage

---
+++ src/app/components/experience.tsx
@@ -0,0 +1,15 @@
+import experiences from "../data/experience.json";
+import ExperienceItem from "./experienceItem";
+
+export default function Experience() {
+    return (
+        <div className="pl-20 pr-10 pb-10">
+            <h1 id="experience-tag" className="font-bold text-3xl pb-10">
+                Experience
+            </h1>
+            {experiences.map((experience) => (
+                <ExperienceItem key={experience.company} experience={experience} />
+            ))}
+        </div>
+    );
+}
---
+++ src/app/components/experienceItem.tsx
@@ -0,0 +1,34 @@
+import { ExperienceItemType } from "../types";
+
+export default function ExperienceItem(props: {
+    experience: ExperienceItemType;
+}) {
+    const { title, location, company, tenure, accomplishments, skills } =
+        props.experience;
+    const accomplishment_number = 0;
+
+    return (
+        <div>
+            <div className="flex flex-row justify-between">
+                <div>
+                    <h1 className="font-bold text-2xl">{company}</h1>
+                    <h1 className="text-sm">{title}</h1>
+                </div>
+                <div>
+                    <h1 className="text-sm">{tenure}</h1>
+                    <h1 className="text-sm">{location}</h1>
+                </div>
+            </div>
+            <ul className="m-4 list-disc">
+                {accomplishments.map(accomplishment => (
+                    <li className="ml-4 m-2">{accomplishment}</li>
+                ))}
+            </ul>
+            <div className="flex mb-8">
+                {skills.map(skill => (
+                    <div className="ml-3 pb-2 pt-2 pr-1 pl-1 rounded-md bg-teal-500 text-sky-950" key={skill}>{skill}</div>
+                ))}
+            </div>
+        </div>
+    );
+}
